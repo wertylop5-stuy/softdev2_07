726b1996959d592dd92057454ea89d1d17851f07
Stanley Lin <slin55055@gmail.com>
2018-02-28 18:07:37 -0500

    finished

--- haha.js
+++ haha.js
@@ -1,45 +1,61 @@
 "use strict";
 
-const SVG_NS = "https://www.w3.org/2000/svg";
+const SVG_NS = "http://www.w3.org/2000/svg";
 
-let circle;
+let svg, circle;
 
-function Circle(x1, y1, e, n, l) {
-	this.elem = e;
+function Circle(x1, y1) {
+	this.elem = null;
 	this.x = x1;
 	this.y = y1;
 	
-	this.next = n;
-	this.line = l;
+	this.next = null;
+	this.line = null;
 }
 
-Circle.prototype.addNext = (e) => {
+function addNext(e) {
 	let x = e.offsetX;
 	let y = e.offsetY;
 	
-	let n = new Circle();
+	let n = new Circle(x, y);
+
 	n.elem = document.createElementNS(SVG_NS, "circle");
-	n.elem.setAttribute("cx");
-	n.elem.setAttribute("cy");
-	n.elem.setAttribute("r");
-	
-	n.line = document.createElementNS(SVG_NS, "line");
-	n.line.setAttribute("x1", x);
-	n.line.setAttribute("y1", y);
-	
-	if (circle) {
+	n.elem.setAttribute("cx", x);
+	n.elem.setAttribute("cy", y);
+	n.elem.setAttribute("r", 10);
+	n.elem.setAttribute("fill", "BlanchedAlmond");
+		
+	if (circle !== undefined) {
+		n.line = document.createElementNS(SVG_NS, "line");
+		n.line.setAttribute("x1", x);
+		n.line.setAttribute("y1", y);
+		n.line.setAttribute("stroke", "black");
 		n.line.setAttribute("x2", circle.x);
 		n.line.setAttribute("y2", circle.y);
+		
+		svg.appendChild(n.line);
 	}
+	
+	svg.appendChild(n.elem);
 	
 	n.next = circle;
 	circle = n;
-};
+}
+
+function clearAll() {
+	let temp;
+	while (circle !== undefined) {
+		if (circle.line !== null) svg.removeChild(circle.line);
+		svg.removeChild(circle.elem);
+		circle = circle.next;
+	}
+}
 
 (() => {
-	let svg = document.getElementById("boi");
 	let clear = document.getElementById("clear");
 	
-	svg.addEventListener("click", circle.addNext);
+	svg = document.getElementById("boi");
+	svg.addEventListener("click", addNext);
+	clear.addEventListener("click", clearAll);
 })()
 
